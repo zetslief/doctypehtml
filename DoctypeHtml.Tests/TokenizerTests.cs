e0b5cd6b78f134823e965c0f15dbbd6e6153588c
Konstantin Konstantin <zetslief@protonmail.com>
2026-02-08 00:16:44 +0100

    Tests: cleanup test project

---
+++ DoctypeHtml.Tests/TokenizerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace DoctypeHtml.Tests;
+
+public class TokenizerTests
+{
+    [Test]
+    public void Basic()
+    {
+        var content = File.ReadAllText("./TestData/Basic.html");
+        Console.WriteLine(content);
+        Assert.Fail("Not implemented");
+    }
+}
