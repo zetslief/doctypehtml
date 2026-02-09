dfc8c9ee1476d9edcc94f901da3ce008e339142b
Konstantin Konstantin <zetslief@protonmail.com>
2026-02-09 01:43:53 +0100

    Tokenizer: start processing markup declaration open state

--- DoctypeHtml.Parser/Tokenizer.cs
+++ DoctypeHtml.Parser/Tokenizer.cs
@@ -13,12 +13,12 @@ internal class Context(ReadOnlyMemory<char> content, Action<Token> emitCallback)
     private readonly ReadOnlyMemory<char> _content = content;
     private readonly Action<Token> _onEmit = emitCallback;
     private int _cursor = 0;
-    
+
     public bool EndOfContent => _content.Length - 1 <= _cursor;
     public Tokenizer.State State { get; set; } = Tokenizer.State.Data;
-    
+
     public void Emit(Token token) => _onEmit(token);
-        
+
     public bool TryConsumeNextInput([NotNullWhen(true)] out char? character)
     {
         if (EndOfContent)
@@ -26,10 +26,22 @@ internal class Context(ReadOnlyMemory<char> content, Action<Token> emitCallback)
             character = null;
             return false;
         }
-        
+
         character = _content.Span[_cursor++];
         return true;
     }
+
+    public ReadOnlyMemory<char>? TryPeek(int count) => _content.Length > _cursor + count
+        ? _content[_cursor..(_cursor + count)]
+        : null;
+
+    public void Consume(int count)
+    {
+        if (_content.Length <= _cursor + count) throw new InvalidOperationException($"Consuming more than available.");
+        _cursor += count;
+    }
+
+    public override string ToString() => $"Context ({_content.Length}): Cursor {_cursor}, State {State}";
 }
 
 public static class Tokenizer
@@ -38,19 +50,20 @@ public static class Tokenizer
     {
         Context context = new(content, tokenEmitCallback);
         do ProcessNextToken(context);
-        while (context.EndOfContent);
+        while (!context.EndOfContent);
     }
-    
+
     private static void ProcessNextToken(Context context)
     {
-        switch (context.State) 
+        switch (context.State)
         {
             case State.Data: ProcessDataState(context); break;
             case State.TagOpen: ProcessTagOpenState(context); break;
-            default: throw new NotImplementedException($"{context}");
-        };
+            case State.MarkupDeclarationOpen: ProcessMarkupDeclarationOpen(context); break;
+            default: throw new NotImplementedException($"Unknown state: {context}");
+        }
     }
-    
+
     private static void ProcessDataState(Context context)
     {
         if (!context.TryConsumeNextInput(out var currentInput))
@@ -64,10 +77,10 @@ public static class Tokenizer
                 context.State = State.TagOpen;
                 return;
             default:
-                throw new NotImplementedException($"{nameof(ProcessDataState)} cannot handle '{currentInput}'({(int)currentInput}) yet");
+                throw new NotImplementedException($"{nameof(ProcessDataState)} cannot handle '{currentInput}' yet.");
         }
     }
-    
+
     private static void ProcessTagOpenState(Context context)
     {
         if (!context.TryConsumeNextInput(out var currentInput))
@@ -75,8 +88,29 @@ public static class Tokenizer
             context.Emit(Token.CreateEndOfFileToken());
             return;
         }
+        switch (currentInput)
+        {
+            case '!':
+                context.State = State.MarkupDeclarationOpen;
+                return;
+            default:
+                throw new NotImplementedException($"{nameof(ProcessDataState)} cannot handle '{currentInput}' yet.");
+        }
     }
-    
+
+    private static void ProcessMarkupDeclarationOpen(Context context)
+    {
+        const string doctype = "DOCTYPE";
+        var maybeDoctype = context.TryPeek(doctype.Length);
+        if (maybeDoctype is not null && maybeDoctype.Value.Span.Equals(doctype.AsSpan(), StringComparison.InvariantCultureIgnoreCase))
+        {
+            context.Consume(doctype.Length);
+            context.State = State.Doctype;
+            return;
+        }
+        else throw new NotImplementedException($"{nameof(ProcessMarkupDeclarationOpen)}");
+    }
+
     public enum State
     {
         Data,
