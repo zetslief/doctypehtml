2751422fb94eb0da1e7e56495ecb19fc7eebd367
Konstantin Konstantin <zetslief@protonmail.com>
2026-02-15 23:10:41 +0100

    Tokenizer: add attributes test

--- DoctypeHtml.Tests/TokenizerTests.cs
+++ DoctypeHtml.Tests/TokenizerTests.cs
@@ -10,4 +10,11 @@ public class TokenizerTests
         var content = File.ReadAllText("./TestData/Basic.html").AsMemory();
         Tokenizer.Run(content, Console.WriteLine);
     }
+
+    [Test]
+    public void Attributes()
+    {
+        var content = File.ReadAllText("./TestData/Attributes.html").AsMemory();
+        Tokenizer.Run(content, Console.WriteLine);
+    }
 }
